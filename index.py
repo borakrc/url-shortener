fd5dae6210c56151c74a66e5b6bec575f68047cd
Bora Kirca <kirca.bora@gmail.com>
2022-06-04 18:29:19 +0200

    doc<Deployment>: explain the unused import in a comment.

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from src.app import serve
-from src.app import app
+from src.app import app  # this line enables the serverless functions discover the Flask App.
 
 if __name__ == '__main__':
     serve()
