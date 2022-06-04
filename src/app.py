5735e3f6b79127e0006a66ed3bc4618bf4cdda32
Bora Kirca <kirca.bora@gmail.com>
2022-06-04 18:40:11 +0200

    feature<Firestore>: initial handshake with firestore.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 
+from src.Controllers.FirestoreTestController import FirestoreTestController
 from src.Controllers.HelloWorldController import HelloWorldController
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorldController, '/')
+api.add_resource(FirestoreTestController, '/dbtest')
 
 def serve():
     app.run(debug=True, host='0.0.0.0', port=5001)
