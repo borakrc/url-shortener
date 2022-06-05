9b7c8a2ab821a1f442ebfaa031f4be3eff8c27fc
Bora Kirca <kirca.bora@gmail.com>
2022-06-06 00:19:33 +0200

    feature<ShortenUrl>: controller logic to create short url.

--- src/Controllers/ShortUrlController.py
+++ src/Controllers/ShortUrlController.py
@@ -2,6 +2,8 @@ from flask_restful import Resource
 from flask import request
 from src.Controllers.Helpers.ExceptionHandler import ExceptionHandler
 from src.Controllers.Helpers.TokenRequired import TokenRequired
+from src.Models.UrlKeyModel import UrlKeyModel
+from src.Models.UrlModel import UrlModel
 from src.config import Config
 
 class ShortUrlController(Resource):
@@ -11,7 +13,7 @@ class ShortUrlController(Resource):
     @ExceptionHandler
     @TokenRequired
     def put(self):
-        longUrl: str = request.form['longUrl']
-        Config.urlShortenerService.createShortUrl(longUrl)
+        longUrl: UrlModel = UrlModel(request.form['longUrl'])
+        shortUrl: UrlKeyModel = Config.urlShortenerService.createShortUrl(longUrl)
 
-        return {'res': 'res'}
+        return {'shortUrl': shortUrl.toPath()}
